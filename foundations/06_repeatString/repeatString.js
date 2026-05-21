eec93f0b9d79bd8c4dbe17d29415b29df46dffb1
Michael Nichols <mjnichols10@yahoo.com>
2026-05-21 18:37:46 -0500

    Solve foundations/06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+  if (n < 0)
+    return "ERROR";
 
+  s = "";
+  for (let i = 0; i < n; i++)
+    s += str;
+  
+  return s;
 };
 
 // Do not edit below this line
