5e22921a8528776ac5764e013d2fef9bc12fcad6
Michael Nichols <mjnichols10@yahoo.com>
2026-05-22 09:52:47 -0500

    Solve foundations/09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (!(Number.isInteger(a) && a >= 0) || !(Number.isInteger(b) && b >= 0)) {
+    return "ERROR";
+  }
+  let sum = []
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    sum.push(i);
+  }
+  return sum.reduce((acc, v) => acc + v, );
 };
 
 // Do not edit below this line
