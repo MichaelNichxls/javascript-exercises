605a3ef9e88a8ee6fdb6c04f2ff458fa9796643e
Michael Nichols <mjnichols10@yahoo.com>
2026-05-22 15:35:14 -0500

    Solve foundations/13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const filtered = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => "abcdefghijklmnopqrstuvwxyz0123456789".includes(char))
+    .join("");
+  const reversed = filtered.split("").reverse().join("");
+  return filtered === reversed;
 };
 
 // Do not edit below this line
