4b75084af140126446b93ca1216618b3fb3ec872
Michael Nichols <mjnichols10@yahoo.com>
2026-05-22 15:59:52 -0500

    Solve foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  }
+  let curr = 1;
+  let prev = 0;
+  for (let i = 0; i < n; i++) {
+    curr += prev;
+    prev = curr - prev;
+  }
+  return prev;
 };
 
 // Do not edit below this line
