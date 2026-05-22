c43fea9c89472497a3b575647acedf176cd6bab2
Michael Nichols <mjnichols10@yahoo.com>
2026-05-22 16:09:09 -0500

    Definitely solve foundations/16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function (people) {
+  return people.reduce((oldPerson, currPerson) => {
+    const oldAge = getAge(oldPerson.yearOfBirth, oldPerson.yearOfDeath);
+    const currAge = getAge(
+      currPerson.yearOfBirth,
+      currPerson.yearOfDeath,
+    );
+    return oldAge < currAge ? currPerson : oldPerson;
+  });
 };
 
 // Do not edit below this line
